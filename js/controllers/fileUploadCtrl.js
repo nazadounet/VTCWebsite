f7a38c5e1c3cb20bc69f57025fbed5b7718991f1
nazadounet <senouci.o@gmail.com>
2017-08-04 18:18:42 +0200

    ajout du systeme de récupération du nom du file pour chaque input / bug a regler = button upload file ne marche plus

--- js/controllers/fileUploadCtrl.js
+++ js/controllers/fileUploadCtrl.js
@@ -2,9 +2,13 @@ app.controller('FileUploadCtrl', ['$scope', 'FileUploader', function($scope, Fil
 
 	       var uploader = $scope.uploader = new FileUploader();
 
+	       $scope.inputName = {
+	       	
+	       };
+
 	       uploader.onAfterAddingFile = function(item){
-	       	console.log(item);
-	       }
-
-
+	       	console.log(document.getElementById('file1').files["0"].name);
+	       	console.log(uploader);
+	       	//$scope.item = document.getElementById('file1').files.FileList;
+	       }			
 }]);
