9cf9a2e12de809022fb47aec3674386c4c5e6527
nazadounet <senouci.o@gmail.com>
2017-08-06 06:11:14 +0200

    mise a jour du système d'upload, phase d'essaie, arret au moment de faire concordé les variable JS avec elle du

--- js/controllers/fileUploadCtrl.js
+++ js/controllers/fileUploadCtrl.js
@@ -1,14 +1,69 @@
-app.controller('FileUploadCtrl', ['$scope', 'FileUploader', function($scope, FileUploader){
+app.controller('FileUploadCtrl', ['$scope', 'FileUploader', function($scope, FileUploader, $window){
 
-	       var uploader = $scope.uploader = new FileUploader();
+	var uploader = $scope.uploader = new FileUploader();
+/*
+		{"VTCCard" : false, "VTCCardIndex" : false},
+		{"IDCard" : false, "IDCardIndex" : false},
+		{"driveCard" : false, "driveCardIndex" : false},
+		{"civilInsurance" : false, "civilInsuranceIndex" : false,}
+		{"atoutFrance" : false, "atoutFranceIndex" : false},
+		{"KBIS" : false, "KBISIndex" : false}
+		function arrayObjectIndexOf(myArray, searchTerm, property) {
+		    for(var i = 0, len = myArray.length; i < len; i++) {
+		        if (myArray[i][property] === searchTerm) return i;
+		    }
+		    return -1;
+		}
+		arrayObjectIndexOf(arr, "stevie", "hello"); // 1
+		*/
 
-	       $scope.inputName = {
-	       	
-	       };
+	$scope.displayFileData = function(){
+		console.log($scope.fileData);
+	}
+/***************************************************** ADDING FILE SECTION ***********************************/
+	$scope.removeItemFromQueue = function (item, id) {
+		uploader.removeFromQueue(item, id);
+	}
 
-	       uploader.onAfterAddingFile = function(item){
-	       	console.log(document.getElementById('file1').files["0"].name);
-	       	console.log(uploader);
-	       	//$scope.item = document.getElementById('file1').files.FileList;
-	       }			
+	$scope.VTCCardFile = function(){
+		uploader.onAfterAddingFile = function(item){
+			$scope.VTCCardId = uploader.getIndexOfItem(item); 
+			$scope.VTCCardFileName = item.file.name;
+		}
+	}
+
+	$scope.IDCardFile = function(){
+		uploader.onAfterAddingFile = function(item){
+			$scope.IDCardId = uploader.getIndexOfItem(item);
+			$scope.IDCardFileName = item.file.name;
+		}
+	}
+
+	$scope.driveCardFile = function(){
+		uploader.onAfterAddingFile = function(item){
+			$scope.driveCardId = uploader.getIndexOfItem(item);
+			$scope.driveCardFileName = item.file.name;
+		}
+	}
+
+	$scope.civilInsuranceFile = function(){
+		uploader.onAfterAddingFile = function(item){
+			$scope.civileInsuranceId = uploader.getIndexOfItem(item);
+			$scope.civileInsuranceFileName = item.file.name;
+		}
+	}
+	$scope.atoutFranceFile = function(){
+		uploader.onAfterAddingFile = function(item){
+			$scope.atoutFranceId = uploader.getIndexOfItem(item);
+			$scope.atoutFranceFileName = item.file.name;
+		}
+	}
+	$scope.KBISFile = function(){
+		uploader.onAfterAddingFile = function(item){
+			$scope.KBISId = uploader.getIndexOfItem(item);
+			$scope.KBISFileName = item.file.name;
+		}
+	}
+/***************************************************** ADDING FILE SECTION ***********************************/
+
 }]);
