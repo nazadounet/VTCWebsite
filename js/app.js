0625ec5b38f0c383a88addec9e0930fa92a73462
nazadounet <senouci.o@gmail.com>
2017-08-07 03:43:07 +0200

    need correction d'un bug qui arrive lorsque l'on uplaod plusieur fichier sur le meme input

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var app = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ui.bootstrap', 'angularFileUpload']);
 
+app.constant('_', window._)
+
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/', {
@@ -11,3 +13,8 @@ app.config(['$routeProvider', function($routeProvider) {
 
 		.otherwise({redirectTo: '/'});
 }]);
+
+  // use in views, ng-repeat="x in _.range(3)"
+ app.run(function ($rootScope) {
+     $rootScope._ = window._;
+  });
--- js/controllers/fileUploadCtrl.js
+++ js/controllers/fileUploadCtrl.js
@@ -1,67 +1,135 @@
-app.controller('FileUploadCtrl', ['$scope', 'FileUploader', function($scope, FileUploader, $window){
+app.controller('FileUploadCtrl', ['$scope', 'FileUploader', '_', function($scope, FileUploader, _){
 
 	var uploader = $scope.uploader = new FileUploader();
-/*
-		{"VTCCard" : false, "VTCCardIndex" : false},
-		{"IDCard" : false, "IDCardIndex" : false},
-		{"driveCard" : false, "driveCardIndex" : false},
-		{"civilInsurance" : false, "civilInsuranceIndex" : false,}
-		{"atoutFrance" : false, "atoutFranceIndex" : false},
-		{"KBIS" : false, "KBISIndex" : false}
-		function arrayObjectIndexOf(myArray, searchTerm, property) {
-		    for(var i = 0, len = myArray.length; i < len; i++) {
-		        if (myArray[i][property] === searchTerm) return i;
-		    }
-		    return -1;
-		}
-		arrayObjectIndexOf(arr, "stevie", "hello"); // 1
-		*/
 
-	$scope.displayFileData = function(){
-		console.log($scope.fileData);
-	}
+	$scope.fileData =  {};
+
 /***************************************************** ADDING FILE SECTION ***********************************/
-	$scope.removeItemFromQueue = function (item, id) {
-		uploader.removeFromQueue(item, id);
+	$scope.removeItemFromQueue = function (fileId) {
+		//retrieving index of a id file to be able to clear file's name in scope.
+		var fileIndex = _.findKey($scope.fileData, ['id', fileId]);
+		//removing from queue with fileId params given to the function
+		uploader.removeFromQueue(fileId);
+		//if filename match with one of all fileData's property, then change $scope Value for this file
+		 for(var filename in $scope.fileData){
+			if (filename == fileIndex){
+				switch(filename){
+					case 'VTCCard' :
+						delete $scope.fileData.VTCCard
+					break;
+
+					case 'IDCard' :
+						delete $scope.fileData.IDCard
+					break;
+
+					case 'driveCard' :
+						delete $scope.fileData.driveCard
+					break;
+
+					case 'civilInsurance' :
+						delete $scope.fileData.civilInsurance
+					break;
+
+					case 'atoutFrance' :
+						delete $scope.fileData.atoutFrance
+					break;
+
+					case 'KBIS' :
+						delete $scope.fileData.KBIS
+					break;
+
+					default:
+						alert('Aucun document ne correspond à votre demande');	
+				}
+			}else{
+				console.log('Aucune référence trouver dans le document')
+			}
+		 }
 	}
 
 	$scope.VTCCardFile = function(){
-		uploader.onAfterAddingFile = function(item){
-			$scope.VTCCardId = uploader.getIndexOfItem(item); 
-			$scope.VTCCardFileName = item.file.name;
+		//firstly we check if there is already a file for this section
+		if($scope.fileData.VTCCard){ 
+			//if true, remove file from queue to avoid id generation problems
+			uploader.removeFromQueue($scope.fileData.VTCCard.id);
+			//clear the $scope file name
+			delete $scope.fileData.VTCCard;
+			//then update the queue and $scope file name
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.VTCCard = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
+		}else{		
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.VTCCard = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
 		}
 	}
 
 	$scope.IDCardFile = function(){
-		uploader.onAfterAddingFile = function(item){
-			$scope.IDCardId = uploader.getIndexOfItem(item);
-			$scope.IDCardFileName = item.file.name;
+		if($scope.fileData.IDCard){ 
+			uploader.removeFromQueue($scope.fileData.IDCard.id);
+			delete $scope.fileData.IDCard;
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.IDCard = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
+		}else{		
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.IDCard = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
 		}
 	}
 
 	$scope.driveCardFile = function(){
-		uploader.onAfterAddingFile = function(item){
-			$scope.driveCardId = uploader.getIndexOfItem(item);
-			$scope.driveCardFileName = item.file.name;
+		if($scope.fileData.driveCard){ 
+			uploader.removeFromQueue($scope.fileData.driveCard.id);
+			delete $scope.fileData.driveCard;
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.driveCard = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
+		}else{		
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.driveCard = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
 		}
 	}
 
 	$scope.civilInsuranceFile = function(){
-		uploader.onAfterAddingFile = function(item){
-			$scope.civileInsuranceId = uploader.getIndexOfItem(item);
-			$scope.civileInsuranceFileName = item.file.name;
+		if($scope.fileData.civilInsurance){ 
+			uploader.removeFromQueue($scope.fileData.civilInsurance.id);
+			delete $scope.fileData.civilInsurance;
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.civilInsurance = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
+		}else{		
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.civilInsurance = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
 		}
 	}
 	$scope.atoutFranceFile = function(){
-		uploader.onAfterAddingFile = function(item){
-			$scope.atoutFranceId = uploader.getIndexOfItem(item);
-			$scope.atoutFranceFileName = item.file.name;
+		if($scope.fileData.atoutFrance){ 
+			uploader.removeFromQueue($scope.fileData.atoutFrance.id);
+			delete $scope.fileData.atoutFrance;
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.atoutFrance = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
+		}else{		
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.atoutFrance = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
 		}
 	}
 	$scope.KBISFile = function(){
-		uploader.onAfterAddingFile = function(item){
-			$scope.KBISId = uploader.getIndexOfItem(item);
-			$scope.KBISFileName = item.file.name;
+		if($scope.fileData.KBIS){ 
+			uploader.removeFromQueue($scope.fileData.KBIS.id);
+			delete $scope.fileData.KBIS;
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.KBIS = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
+		}else{		
+			uploader.onAfterAddingFile = function(item){
+				$scope.fileData.KBIS = {'filename' : item.file.name, 'id' : uploader.getIndexOfItem(item)};
+			}
 		}
 	}
 /***************************************************** ADDING FILE SECTION ***********************************/
